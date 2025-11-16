b9b70d3a48f0449eebde7c04bbd182a2d781c00f
karmenott <karmen.ott@ut.ee>
2025-11-16 23:29:04 +0200

    Remove dataconnect-generated from gitignore and add it to sources

---
+++ src/dataconnect-generated/index.d.ts
@@ -0,0 +1,62 @@
+import { ConnectorConfig, DataConnect, QueryRef, QueryPromise } from 'firebase/data-connect';
+
+export const connectorConfig: ConnectorConfig;
+
+export type TimestampString = string;
+export type UUIDString = string;
+export type Int64String = string;
+export type DateString = string;
+
+
+
+
+export interface CoinByIdData {
+  coinMetas: ({
+    id: Int64String;
+    muisId: number;
+    title: string;
+    description?: string | null;
+    date?: string | null;
+    diameterMm: number;
+  } & CoinMeta_Key)[];
+}
+
+export interface CoinByIdVariables {
+  id: Int64String;
+}
+
+export interface CoinCountData {
+  coinMeta?: {
+    id_count: number;
+  };
+}
+
+export interface CoinMeta_Key {
+  muisId: number;
+  __typename?: 'CoinMeta_Key';
+}
+
+interface CoinByIdRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (vars: CoinByIdVariables): QueryRef<CoinByIdData, CoinByIdVariables>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect, vars: CoinByIdVariables): QueryRef<CoinByIdData, CoinByIdVariables>;
+  operationName: string;
+}
+export const coinByIdRef: CoinByIdRef;
+
+export function coinById(vars: CoinByIdVariables): QueryPromise<CoinByIdData, CoinByIdVariables>;
+export function coinById(dc: DataConnect, vars: CoinByIdVariables): QueryPromise<CoinByIdData, CoinByIdVariables>;
+
+interface CoinCountRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (): QueryRef<CoinCountData, undefined>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect): QueryRef<CoinCountData, undefined>;
+  operationName: string;
+}
+export const coinCountRef: CoinCountRef;
+
+export function coinCount(): QueryPromise<CoinCountData, undefined>;
+export function coinCount(dc: DataConnect): QueryPromise<CoinCountData, undefined>;
+
